92c790a49c9ce2acb1a1db6ff45b828001f51091
rudchyk <sergii.rudchyk@gmail.com>
2022-06-21 16:15:26 +0300

    steps

--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,50 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let i = 1; i <= n; i++) {
+    const arr = Array(n).fill(' ');
+
+    for (let j = 0; j < i; j++) {
+      arr[j] = '#';
+    }
+
+    console.log(arr.join(''));
+  }
+}
+
+function steps1(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = '';
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
+    }
+    console.log(stair);
+  }
+}
+
+function steps2(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  steps(n, row, stair);
+}
 
 module.exports = steps;
