812770ef44e6713d8b5b9a24e241cdfbfef63aef
rudchyk <sergii.rudchyk@gmail.com>
2022-06-21 12:32:46 +0300

    first

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 https://www.udemy.com/course/coding-interview-bootcamp-algorithms-and-data-structure/learn/lecture/8533050#overview
+
+1. `debugger` break
+2. execute function
+3. terminal:
+
+```
+node inspect reversestring/index.js
+```
+
+`$ c` => continue
+`$ repl` => replication mode
--- reversestring/index.js
+++ reversestring/index.js
@@ -6,6 +6,32 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reduce((accumulator, item) => item + accumulator, '');
+}
+
+function reverse1(str) {
+  return str.split('').reverse().join('');
+}
+
+function reverse2(str) {
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    result = str[i] + result;
+  }
+
+  return result;
+}
+
+function reverse3(str) {
+  let result = '';
+
+  for (let iterator of str) {
+    result = iterator + result;
+  }
+
+  return result;
+}
 
 module.exports = reverse;
