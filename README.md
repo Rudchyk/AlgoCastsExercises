9b1172b62c959139fe6469cf6066d38e7b8c71e0
rudchyk <sergii.rudchyk@gmail.com>
2022-06-21 15:02:07 +0300

    added

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 https://www.udemy.com/course/coding-interview-bootcamp-algorithms-and-data-structure/learn/lecture/8533050#overview
 
 https://playcode.io/javascript
+https://stephengrider.github.io/JSPlaygrounds/
 
 1. `debugger` break
 2. execute function
--- anagrams/index.js
+++ anagrams/index.js
@@ -8,6 +8,56 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const getObj = (string) => {
+    const str = string.replace(/[^\w]/g, '').toLowerCase();
+    const obj = {};
 
-module.exports = anagrams;
+    for (const iterator of str) {
+      if (!obj[iterator]) {
+        obj[iterator] = 1;
+      } else {
+        obj[iterator] += 1;
+      }
+    }
+
+    return obj;
+  };
+  const objA = getObj(stringA);
+  const objB = getObj(stringB);
+
+  if (Object.keys(objA).length !== Object.keys(objB).length) {
+    return false;
+  }
+
+  // let result = false;
+
+  // for (const key in objA) {
+  //   if (objA[key] === objB[key]) {
+  //     result = true;
+  //   } else {
+  //     result = false;
+  //   }
+  // }
+
+  // return result;
+
+  for (const key in objA) {
+    if (objA[key] !== objB[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function anagrams1(stringA, stringB) {
+  const prepareStr = (str) => {
+    const formattedStr = str.replace(/[^\w]/g, '').toLowerCase();
+    return formattedStr.split('').sort().join('');
+  };
+
+  return prepareStr(stringA) === prepareStr(stringB);
+}
+
+module.exports = anagrams1;
