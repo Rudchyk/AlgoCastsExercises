91cff3388d23122f882fb2e9d2d92f84b21c824d
rudchyk <sergii.rudchyk@gmail.com>
2022-06-21 13:44:38 +0300

    added

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 https://www.udemy.com/course/coding-interview-bootcamp-algorithms-and-data-structure/learn/lecture/8533050#overview
 
+https://playcode.io/javascript
+
 1. `debugger` break
 2. execute function
 3. terminal:
--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,41 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const obj = {};
+
+  // for (let i = 0; i < str.length; i++) {
+  //   const char = str[i];
+  //   if (!obj[char]) {
+  //     obj[char] = 1;
+  //   } else {
+  //     obj[char] = obj[char] + 1;
+  //   }
+  // }
+
+  for (const char of str) {
+    // if (!obj[char]) {
+    //   obj[char] = 1;
+    // } else {
+    //   obj[char] = obj[char] + 1;
+    // }
+
+    obj[char] = obj[char] + 1 || 1;
+  }
+
+  let resultCount = 0;
+  let result = '';
+
+  for (const key in obj) {
+    if (obj[key] >= resultCount) {
+      resultCount = obj[key];
+      result = key;
+    }
+  }
+
+  return result;
+}
+
+function maxChar1(str) {}
 
 module.exports = maxChar;
