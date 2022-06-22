76c513ee710e79588e72a2683b1f5fc1131e31d8
rudchyk <sergii.rudchyk@gmail.com>
2022-06-22 13:36:41 +0300

    vowels

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,49 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let counter = 0;
+  const r = new RegExp(/[aeiou]/, 'm');
 
-module.exports = vowels;
+  for (const letter of str.toLowerCase()) {
+    if (r.test(letter)) {
+      counter++;
+    }
+  }
+
+  return counter;
+}
+
+function vowels1(str) {
+  let counter = 0;
+  const vowelsArr = ['a', 'e', 'i', 'o', 'u'];
+
+  for (const letter of str.toLowerCase()) {
+    if (vowelsArr.includes(letter)) {
+      counter++;
+    }
+  }
+
+  return counter;
+}
+
+function vowels2(str) {
+  let counter = 0;
+  const vowels = 'aeiou';
+
+  for (const letter of str.toLowerCase()) {
+    if (vowels.includes(letter)) {
+      counter++;
+    }
+  }
+
+  return counter;
+}
+
+function vowels3(str) {
+  const matches = str.match(/[aeiou]/gi);
+
+  return matches ? matches.length : 0;
+}
+
+module.exports = vowels2;
