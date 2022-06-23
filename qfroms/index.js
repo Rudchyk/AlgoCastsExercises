f5bd21bad796236bc547ecdf4c51da3483babb9e
rudchyk <sergii.rudchyk@gmail.com>
2022-06-23 14:31:15 +0300

    qfroms1

--- qfroms/index.js
+++ qfroms/index.js
@@ -53,4 +53,43 @@ class Queue {
   }
 }
 
-module.exports = Queue;
+class Queue1 {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+
+  add(record) {
+    this.stack1.push(record);
+  }
+
+  remove() {
+    while (this.stack1.peek()) {
+      this.stack2.push(this.stack1.pop());
+    }
+
+    const record = this.stack2.pop();
+
+    while (this.stack2.peek()) {
+      this.stack1.push(this.stack2.pop());
+    }
+
+    return record;
+  }
+
+  peek() {
+    while (this.stack1.peek()) {
+      this.stack2.push(this.stack1.pop());
+    }
+
+    const record = this.stack2.peek();
+
+    while (this.stack2.peek()) {
+      this.stack1.push(this.stack2.pop());
+    }
+
+    return record;
+  }
+}
+
+module.exports = Queue1;
